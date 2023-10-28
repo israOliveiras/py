b75d6bc157a50817c2f6d70c49d19b9e443b5f75
israOliveiras <149240635+israOliveiras@users.noreply.github.com>
2023-10-28 07:58:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const pergunta = [
+    {
+        pergunta:"Não sei kk, dps a gente vê",
+        respostas:[
+            {text:"sla não sei que pergunta colocar", correta: false },
+            {text:"sla2", correta: false },
+            {text:"sla3", correta: true },
+            {text:"sla4", correta: false },
+        ]
+    },
+    {
+        pergunta:"Não sei kk, dps a gente vê",
+        respostas:[
+            {text:"sla não sei que pergunta colocar", correta: false },
+            {text:"sla2", correta: false },
+            {text:"sla3", correta: true },
+            {text:"sla4", correta: false },
+        ]
+    },
+
+
+];
